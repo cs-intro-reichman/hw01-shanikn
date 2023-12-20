25dfaea1a9b2aa44a3b3d9c23091d9570f17b311
Shani Knobel <shani.knobel@post.runi.ac.il>
2023-12-20 23:22:20 +0200

    removing unnecessary text

--- Coins.java
+++ Coins.java
@@ -8,6 +8,6 @@ public class Coins
 		int x= Integer.parseInt(args[0]);
 		int y= x/25;
 		int z= x%25;
-		System.out.println("The quantity of quarters you can have from the sum of the quantity of the cents you have entered is "+y+", and the quantity of the remainer of the cents is "+z );
+		System.out.println("Use "+y+" quarters and "+z+" cents" );
 	}
 }
