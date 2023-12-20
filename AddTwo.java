b1566728c61572b4bb3dfc6fb820ad03139d9a09
Shani Knobel <shani.knobel@post.runi.ac.il>
2023-12-20 23:18:31 +0200

    removing the unnecessary tex

--- AddTwo.java
+++ AddTwo.java
@@ -8,7 +8,8 @@ public class AddTwo
        int x= Integer.parseInt(args[0]);
        int y= Integer.parseInt(args[1]);
 
-       System.out.println("the answer is: "+x + " + " + y + " = " +(x+y));
+       System.out.println(x + " + " + y + " = " +(x+y));
+       
     }
 
 }
